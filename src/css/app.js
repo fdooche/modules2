bb0ad66876bd2b67dc2ced47dcc77ddbe4fadf0e
fdooch <amigoist@yandex.ru>
2024-09-12 11:25:20 +0300

    init2

---
+++ src/css/app.js
@@ -0,0 +1,5 @@
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './js/game';
+
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+export default class Game {
+    start() {
+          console.log('game started');
+      
+  }
+  
+}
+
+export class GameSavingData {
+  
+}
+11
+export function readGameSaving() {
+  
+}
+14
+export function writeGameSaving() {
+  
+}
